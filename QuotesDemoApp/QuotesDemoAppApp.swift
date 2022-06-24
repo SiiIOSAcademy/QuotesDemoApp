c2d15cd47f2be4e9a8384c4c1871f0104be0e59e
Sebastian Hat <shat@sii.pl>
2022-06-24 15:43:58 +0200

    Initial Commit

---
+++ QuotesDemoApp/QuotesDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesDemoAppApp.swift
+//  QuotesDemoApp
+//
+//  Created by Sebastian Hat on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuotesDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
